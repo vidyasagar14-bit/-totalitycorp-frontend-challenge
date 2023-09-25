568907ee763a8a4cd93659426ef7976998cba678
Vidyasagarpatil <97180476+vidyasagar14-bit@users.noreply.github.com>
2023-09-25 19:12:59 +0530

    Update server.js
    updated changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path"
+import {fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -15,6 +16,9 @@ dotenv.config();
 //databse config
 connectDB();
 
+//esmodeule fix
+const __filename= fileURLToPath(import.meta.url);
+const __dirname=path.__dirname(__filename);
 //rest object
 const app = express();
 
